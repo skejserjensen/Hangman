26667ca30955c25ecb8535c93238a5497c0b3f05
Soeren Kejser Jensen <devel@kejserjensen.dk>
2013-05-17 23:06:28 +0200

    Removed MonadRandom as dependency for wordlist

--- haskell/src/WordList.hs
+++ haskell/src/WordList.hs
@@ -3,16 +3,13 @@ module WordList
     getRandomWord
 ) where
 
-import Control.Monad.Random
+import System.Random
 
 getRandomWord :: String -> IO String
-getRandomWord wordListPath = do 
-                wordList <- readWordList wordListPath
-                index <- getRandomIndex wordList
-                return (wordList!!index)
+getRandomWord wordListPath = readWordList wordListPath >>= (\wordList -> getRandomIndex wordList >>= (\index -> return $ wordList !! index))
 
 readWordList :: String -> IO [String]
 readWordList wordListPath = readFile wordListPath >>= (\wordList -> return (words wordList))
 
 getRandomIndex :: [a] -> IO Int
-getRandomIndex wordList = getRandomR (0, length wordList)
+getRandomIndex wordList = randomRIO (0, length wordList)
