32fb325a5076a0ae1771a5d4a0275082ac1c3d8c
Soeren Kejser Jensen <devel@kejserjensen.dk>
2013-08-21 21:57:30 +0200

    All version is now build and run through a makefile

--- php/Run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Just a small bash script to run the php version of hangman, as it does not to be compiled
-# only the first argument is passed along to the script as it can't handle any more anyway
-php ./src/main.php $1
